6ffc8e5594d08a3811e9c3e6e1e1da8be33d81a4
Sugar Shane <scrisostomo17@gmail.com>
2021-07-21 14:39:35 -0400

    completed draft

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,12 +4,10 @@ function renderLicenseBadge(license) {
   if (license === "MIT") {
     return `[License](https://img.shields.io/badge/License-MIT-brightgreen)`
   } else if (license === "GNU") {
-    return `[License]()`
+    return `[License](https://img.shields.io/badge/License-GNU-orange)`
   } else if (license === "Apache") {
-    return `[License]()`
-  } else if (license === "Other") {
-    return `[License]()`
-  } 
+    return `[License](https://img.shields.io/badge/License-Apache-blue)`
+  }
 }
 
 // TODO: Create a function that returns the license link
@@ -26,57 +24,85 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  // console.log(`# ${data.title} 
-
-  // ## Description ${data.description}
-
-  // ## Table of Contents * [Installation](#installation) * [Usage](#usage) * [Credits](#credits) * [License](#license)
-
-  // ## Installations ${data.installation}
-  
-  // ## Usage ${data.usage}
-
-  // ## License ${data.license}
-
-  // ## Contributing ${data.contributing}
-
-  // ## Test ${data.test}
-
-  // ## Questions ${data.question}
-
-  // `);
-
-  return `
-  # ${data.title}
+  console.log(`
+  # Project Title
+  ## ${data.title}
 
   ## Description
-  ${data.description}
+  ### ${data.description}
 
   ## Table of Contents 
-  * [Installation](#installation) 
-  * [Usage](#usage) 
-  * * [Credits](#credits) 
-  * * [License](#license) 
-  
+  - [Description](#description)
+  - [Installation](#installation) 
+  - [Usage](#usage) 
+  - [Contributing](#contributing) 
+  - [Test](#test) 
+  - [License](#license) 
+  - [Questions](#questions)
+
   ## Installations
-  ${data.installation}
+  ### ${data.installation}
+
   ## Usage
-  ${data.usage}
+  ### ${data.usage}
+
   ## License
-  ${data.license}
+  ### ${data.license}
+  ### ${data.license}
+  ### ${data.license}
+
   ## Contributing
-  ${data.contributing}
+  ###${data.contributing}
+
   ## Test
-  ${data.test}
+  ###${data.test}
+
+
+  `);
+
+  return `
+  # Project Title
+  ## ${data.title}
+
+  ## Description
+  ### ${data.description}
+
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation) 
+  - [Usage](#usage) 
+  - [Contributing](#contributing) 
+  - [Test](#test) 
+  - [License](#license) 
+  - [Questions](#questions)
+
+  ## Installations
+  ### ${data.installation}
+
+  ## Usage
+  ### ${data.usage}
+
+  ## License
+  ### ${data.license}
+  ### ${data.license}
+  ### ${data.license}
+
+  ## Contributing
+  ### ${data.contributing}
+
+  ## Test
+  ### ${data.test}
+
   ## Questions
-  ${data.questions}
+  ### For additional questions, please feel free to each out using the links below!
+  ### GitHub UserName: ${data.githubUser}
+  ### GitHub Link: ${data.githubLink}
+  ### E-mail: ${data.email}
 
   `;
-  
 }
 
 
 module.exports = generateMarkdown;
 
 
-//Table of contents  need to modify later
